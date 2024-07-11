8fd0e34ae91d6ef9cf32ded2aaf84f541472769a
GabrielNP96 <gabrielnp1996@gmail.com>
2024-07-11 06:01:02 -0300

    exibindo o cardápio

--- app.py
+++ app.py
@@ -5,12 +5,11 @@ from modelos.cardapio.bebida import Bebida
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida ('Suco de Melancia', 5.0, 'grande')
 prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
-restaurante_praca.adicionar_no_cardapio(bebida_suco)
-restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+restaurante_praca.adcionar_no_cardapio(bebida_suco)
+restaurante_praca.adcionar_no_cardapio(prato_paozinho)
 
 def main():
-    print(bebida_suco)
-    print(prato_paozinho)
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
